7847659169a90c5b4afdf96141e378bfbf737e7c
kervs <kervins.valcourt@gmail.com>
2014-12-25 20:05:08 -0500

    problem details

---
+++ README.md
@@ -0,0 +1,16 @@
+Problem Statement
+
+The Utopian tree goes through 2 cycles of growth every year. The first growth cycle occurs during the spring, when it doubles in height. The second growth cycle occurs during the summer, when its height increases by 1 meter. 
+Now, a new Utopian tree sapling is planted at the onset of the spring. Its height is 1 meter. Can you find the height of the tree after N growth cycles?
+
+Input Format 
+The first line contains an integer, T, the number of test cases. 
+T lines follow. Each line contains an integer, N, that denotes the number of cycles for that test case.
+
+Constraints 
+1 <= T <= 10 
+0 <= N <= 60
+
+Output Format 
+For each test case, print the height of the Utopian tree after N cycles.
+
--- Utopian Tree/main.m
+++ Utopian Tree/main.m
@@ -8,10 +8,44 @@
 
 #import <Foundation/Foundation.h>
 
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        int test,y,i,j,newvalue = 0;
+        NSMutableArray *arrayCycles= [NSMutableArray array];
+        NSMutableArray *arrayNumber = [NSMutableArray array];
+        
+        scanf("%d",&test);
+        
+        
+        for (i =0 ; i < test; i++) {
+            
+            scanf("%d",&y);
+            [arrayCycles addObject:[NSNumber numberWithInt:y]];
+        }
+        
+        
+        for ( j=0 ; j<[arrayCycles count] ;j++)
+        {   NSNumber *tempNumber =[arrayCycles objectAtIndex:j];
+            
+            for (int i = 0; i <= [tempNumber integerValue]; i++) {
+                if (i %2 ==0){
+                    newvalue =newvalue +1;
+                    
+                }else{
+                    newvalue = newvalue*2;
+                }
+            }
+            
+            [arrayNumber addObject:[NSNumber numberWithInt:newvalue]];
+            newvalue = 0;
+            
+        }
+        
+        for (int k = 0; k < [arrayNumber count]; k++) {
+            printf("%ld\n",[(NSNumber *)arrayNumber[k] integerValue]);
+        }
     }
-    return 0;
+    
 }
